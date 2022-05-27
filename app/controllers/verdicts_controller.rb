fa417a5ed68a39f082377a2b97c21f534f255eca
binarygit <inscription4@outlook.com>
2022-05-27 18:00:08 +0545

    create verdicts which determines the match verdict and display it

--- app/controllers/verdicts_controller.rb
+++ app/controllers/verdicts_controller.rb
@@ -1,2 +1,15 @@
 class VerdictsController < ApplicationController
+  def create
+    correct_answer = cookies[:correct_answer]
+    answer = params[:commit]
+
+    case answer
+    when correct_answer
+      flash.notice = 'correct!'
+    else
+      flash.notice = 'incorrect!'
+    end
+
+    redirect_to root_path
+  end
 end
