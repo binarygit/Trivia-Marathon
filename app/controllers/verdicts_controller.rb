d4c84122d69edf39baca62337a04b1c7abacc86e
binarygit <inscription4@outlook.com>
2022-05-28 14:22:22 +0545

    create modal box which closes when cross is clicked

--- app/controllers/verdicts_controller.rb
+++ app/controllers/verdicts_controller.rb
@@ -8,10 +8,18 @@ class VerdictsController < ApplicationController
     when correct_answer
       flash.notice = 'That was correct!'
     else
-      # reset num of questions answered
+      set_cookies
       cookies.delete :questions_answered
+      flash.alert = 'incorrect!'
     end
 
     redirect_to root_path
+  end
+
+  private
+
+  def set_cookies
+    cookies[:total_score] = cookies[:questions_answered]
+    cookies[:correct_ans_for_prev_ques] = cookies[:correct_answer]
   end
 end
